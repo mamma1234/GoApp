2fc7a075c7e8f3c7ec0993cbba93134a410de22f
박대규 <mamma1234@gmail.com>
2019-11-27 10:48:24 +0900

    first

---
+++ src/file_ioutil.go
@@ -0,0 +1,17 @@
+package main
+
+import "io/ioutil"
+
+func main() {
+    //파일 읽기
+    bytes, err := ioutil.ReadFile("C:\\temp\\1.txt")
+    println(bytes)
+    if err != nil {
+        panic(err)
+    }
+    //파일 쓰기
+    err = ioutil.WriteFile("C:\\temp\\2.txt", bytes, 0)
+    if err != nil {
+        panic(err)
+    }
+}
